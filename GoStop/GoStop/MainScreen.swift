a678b732c71461b3725deefdae1800d1da78ec59
taehyunL <taylor1233@naver.com>
2022-01-17 19:03:22 +0900

    MainScreen 앱의 메인이 되는 view

---
+++ GoStop/GoStop/MainScreen.swift
@@ -0,0 +1,37 @@
+//
+//  MainScreen.swift
+//  GoStop
+//
+//  Created by 이태현 on 2022/01/17.
+//
+
+import SwiftUI
+
+struct MainScreen: View {
+    @State var mainScreenOn = false
+    
+    var body: some View {
+        ZStack {
+            MainPage()
+            
+            if !mainScreenOn {
+                LottieAnimationView()
+                    .background(Color.white.edgesIgnoringSafeArea(.all))
+                    .transition(.opacity)
+            }
+        }
+        .onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                withAnimation {
+                    mainScreenOn.toggle()
+                }
+            }
+        }
+    }
+}
+
+struct MainScreen_Previews: PreviewProvider {
+    static var previews: some View {
+        MainScreen()
+    }
+}
