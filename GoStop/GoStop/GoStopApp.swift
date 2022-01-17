cc815a8db0ec7ada90b9aec56ffadc863f7cca4b
taehyunL <taylor1233@naver.com>
2022-01-17 19:02:55 +0900

    mainApp

---
+++ GoStop/GoStop/GoStopApp.swift
@@ -0,0 +1,17 @@
+//
+//  GoStopApp.swift
+//  GoStop
+//
+//  Created by 이태현 on 2022/01/12.
+//
+
+import SwiftUI
+
+@main
+struct GoStopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            MainScreen()
+        }
+    }
+}
