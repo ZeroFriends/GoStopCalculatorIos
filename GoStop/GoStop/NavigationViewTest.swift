cbadd87ae2304aa31a7d1c33bde8370aba4dd6ee
taehyunL <taylor1233@naver.com>
2022-01-19 00:15:33 +0900

    전환용 View

--- GoStop/GoStop/NavigationViewTest.swift
+++ GoStop/GoStop/NavigationViewTest.swift
@@ -9,10 +9,17 @@ import SwiftUI
 
 struct NavigationViewTest: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("Hello")
     }
 }
 
+struct TextView: View {
+    var body: some View {
+        NavigationView {
+            Text("here")
+        }
+    }
+}
 struct NavigationViewTest_Previews: PreviewProvider {
     static var previews: some View {
         NavigationViewTest()
