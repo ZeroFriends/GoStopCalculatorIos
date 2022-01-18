f1ac541e6220cbec757a6587ca31c9e1696a11b8
taehyunL <taylor1233@naver.com>
2022-01-19 01:44:32 +0900

    이름변경

--- GoStop/GoStop/FirstScreen.swift
+++ GoStop/GoStop/FirstScreen.swift
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct MainScreen: View {
+struct FirstScreen: View {
     @State var mainScreenOn = false
     
     var body: some View {
@@ -32,6 +32,6 @@ struct MainScreen: View {
 
 struct MainScreen_Previews: PreviewProvider {
     static var previews: some View {
-        MainScreen()
+        FirstScreen()
     }
 }
