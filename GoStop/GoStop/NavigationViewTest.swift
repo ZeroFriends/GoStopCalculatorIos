3b93d12b1133a25dbb6d5df7767d876f9290fb4d
taehyunL <taylor1233@naver.com>
2022-01-18 22:40:25 +0900

    네이게이션 뷰 테이스용

---
+++ GoStop/GoStop/NavigationViewTest.swift
@@ -0,0 +1,20 @@
+//
+//  NavigationViewTest.swift
+//  GoStop
+//
+//  Created by 이태현 on 2022/01/18.
+//
+
+import SwiftUI
+
+struct NavigationViewTest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct NavigationViewTest_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationViewTest()
+    }
+}
