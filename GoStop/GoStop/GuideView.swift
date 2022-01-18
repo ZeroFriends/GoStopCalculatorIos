db049b7a60241a4c2c3fdeb4140709195dd12f46
taehyunL <taylor1233@naver.com>
2022-01-19 01:33:05 +0900

    rename

--- GoStop/GoStop/GuideView.swift
+++ GoStop/GoStop/GuideView.swift
@@ -7,21 +7,14 @@
 
 import SwiftUI
 
-struct NavigationViewTest: View {
+struct GuideView: View {
     var body: some View {
         Text("Hello")
     }
 }
 
-struct TextView: View {
-    var body: some View {
-        NavigationView {
-            Text("here")
-        }
-    }
-}
 struct NavigationViewTest_Previews: PreviewProvider {
     static var previews: some View {
-        NavigationViewTest()
+        GuideView()
     }
 }
