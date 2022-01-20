67a44bd49b4b80abd4ccd6541c07f026792007e0
taehyunL <taylor1233@naver.com>
2022-01-20 18:12:57 +0900

    Guide화면 구성 첫번째 단계

--- GoStop/GoStop/GuideView.swift
+++ GoStop/GoStop/GuideView.swift
@@ -8,13 +8,67 @@
 import SwiftUI
 
 struct GuideView: View {
+    @ObservedObject var viewModel: MainPageViewModel
+    
     var body: some View {
-        Text("Hello")
+        ZStack {
+            VStack {
+                HStack {
+                    Button {
+                        withAnimation {
+                            viewModel.chooseNavigationButton()
+                        }
+                    } label: {
+                        Image(systemName: "arrow.left")
+                            .foregroundColor(.black)
+                    }
+                    Spacer()
+                    Text("가이드")
+                        .fontWeight(.bold)
+                    Spacer()
+                    Button {
+                        withAnimation {
+                            viewModel.chooseNavigationButton()
+                        }
+                    } label: {
+                        Image(systemName: "multiply")
+                            .foregroundColor(.black)
+                    }
+                }
+                .padding([.top, .leading, .trailing], 25.0)
+                .font(.system(size: 18))
+                ZStack {
+                    Color.red
+                        .ignoresSafeArea(.all, edges: .bottom)
+                    Image("그룹 93")
+                        .offset(y: 70)
+                    VStack {
+                        Spacer()
+                        Button {
+                            
+                        } label: {
+                            ZStack {
+                                RoundedRectangle(cornerRadius: 26)
+                                    .frame(width: 318, height: 52, alignment: .bottom)
+                                    .foregroundColor(.black)
+                                Text("시작하기")
+                                    .foregroundColor(.white)
+                                    .fontWeight(.bold)
+                                    .font(.system(size: 16))
+                            }
+                        }
+                    }
+                }
+            }
+            
+            
+        }
     }
 }
 
 struct NavigationViewTest_Previews: PreviewProvider {
     static var previews: some View {
-        GuideView()
+        let viewModel = MainPageViewModel()
+        GuideView(viewModel: viewModel)
     }
 }
