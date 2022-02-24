2680c17a349ebcb76e5c1af8708cbbae8f4e3a86
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-25 01:40:27 +0900

    다시 첫화면으로 올때 계속 lottieView 실행되는 상황 수정

--- GoStop/GoStop/FirstScreen.swift
+++ GoStop/GoStop/FirstScreen.swift
@@ -11,12 +11,13 @@ struct FirstScreen: View {
     @State var mainScreenOn = false
     @Environment(\.scenePhase) var scenePhase
     @Binding var mainPageHistories: [MainPageHistory]
+    @State var cnt = 0
     let saveAction: ()->Void
     
     
     var body: some View {
         ZStack {
-            MainPage()
+            MainPage(mainPageHistories: $mainPageHistories)
             
             if !mainScreenOn {
                 LottieAnimationView()
@@ -24,21 +25,24 @@ struct FirstScreen: View {
                     .transition(.opacity)
             }
         }
+        .onAppear {
+            if cnt == 0 {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+                    withAnimation {
+                        mainScreenOn.toggle()
+                    }
+                }
+                cnt += 1//이렇게 안하면 첫화면일때 계속 로티이미지 떠버려서 막음
+            }
+        }
         .onChange(of: scenePhase) { phase in
             if phase == .inactive { saveAction() }
-        }
-        .onAppear {
-            DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
-                withAnimation {
-                    mainScreenOn.toggle()
-                }
-            }
         }
     }
 }
 
 struct MainScreen_Previews: PreviewProvider {
-    static var history = MainPageHistory(date: "2022-01-17", historyName: "2021-09-24", rule: CostRule(jumDant: 0, ppuck: 0, tatack: 0, sell: 0))
+    static var history = MainPageHistory(date: "2022-01-17", historyName: "2021-09-24", rule: CostRule())
     static var mainPageHistory: [MainPageHistory] = [history]
     
     static var previews: some View {
