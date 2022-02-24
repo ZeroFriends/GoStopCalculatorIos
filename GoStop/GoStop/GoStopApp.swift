6bdf1ba5c12b967d954ff2fb8dbf9c9636832dcd
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-24 23:37:05 +0900

    앱 실행시작시에 데이터 저장 및 기록

--- GoStop/GoStop/GoStopApp.swift
+++ GoStop/GoStop/GoStopApp.swift
@@ -9,9 +9,30 @@ import SwiftUI
 
 @main
 struct GoStopApp: App {
+    @StateObject var dataStore = DataController()
+    
     var body: some Scene {
         WindowGroup {
-            FirstScreen()
+            NavigationView {
+                FirstScreen(mainPageHistories: $dataStore.mainPageHistories) {
+                    DataController.save(mainPageHistorys: dataStore.mainPageHistories) { result in
+                        if case .failure(let error) = result {
+                            fatalError(error.localizedDescription)
+                        }
+                    }
+                }
+                
+            }
+            .onAppear {
+                DataController.load { result in
+                    switch result {
+                    case .failure(let error):
+                        fatalError(error.localizedDescription)
+                    case .success(let mainPageHistories):
+                        dataStore.mainPageHistories = mainPageHistories
+                    }
+                }
+            }
         }
     }
 }
