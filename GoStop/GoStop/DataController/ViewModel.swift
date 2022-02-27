d05b21f7744af6ea483577cd648fbab5a5d6c4eb
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-02-27 15:21:17 +0900

    view에서 save, load 하지 말고 viewmodel이 역할 하도록

---
+++ GoStop/GoStop/DataController/ViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  ViewModel.swift
+//  GoStop
+//
+//  Created by 이태현 on 2022/02/25.
+//
+
+import Foundation
+import SwiftUI
+
+//class ViewModel: ObservableObject {
+//    @Binding var
+//
+//
+//}
+//여기서 datasourece에 접근을 하고 그 다음에 view에 뿌려야 한다
