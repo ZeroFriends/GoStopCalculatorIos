668a1a7cb0deae23bde9a422e9b26c68c3327645
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-04-24 03:37:23 +0900

    정산내역 완료, ui 수정 남음

--- GoStop/GoStop/View/CalculateView.swift
+++ GoStop/GoStop/View/CalculateView.swift
@@ -10,11 +10,81 @@ import SwiftUI
 struct CalculateView: View {
     
     @Environment(\.presentationMode) var presentationMode
+    var coreDM: CoreDataManager
     var mainPageHistory: MainPageHistory
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-            .navigationBarHidden(true)
+        VStack {
+            HStack {
+                Button {
+                    presentationMode.wrappedValue.dismiss()
+                } label: {
+                    Image(systemName: "arrow.left")
+                        .foregroundColor(.black)
+                }
+                Spacer()
+                Text(mainPageHistory.historyName!)
+                    .font(.system(size: 14))
+                    .bold()
+                Spacer()
+                Image(systemName: "arrow.left")
+                    .opacity(0)
+            }
+            .padding(.horizontal)
+            divideRectangle()
+            HStack {
+                Text("정산내역")
+                    .font(.system(size: 24, weight: .bold))
+                Spacer()
+            }
+            .padding([.top, .leading, .trailing])
+            
+            ScrollView {
+                VStack {
+                    ForEach(mainPageHistory.playerlist, id: \.self) { player in
+                        VStack {
+                            HStack {
+                                Text("\(mainPageHistory.playerlist.firstIndex(of: player)!+1)\t")
+                                    .font(.system(size: 16, weight: .bold))
+                                    .foregroundColor(.red)
+                                Text(player.name ?? "")
+                                Spacer()
+                                let totalCost = coreDM.fetchSpecificPlayerTotalCost(id: mainPageHistory.id ?? UUID())
+                                Text("\(totalCost)")
+                            }
+                            .padding(.bottom, 3)
+                            
+                            ForEach(mainPageHistory.playerlist, id: \.self) { innerPlayer in
+                                let mainName = player.name ?? ""
+                                let enemyName = innerPlayer.name ?? ""
+                                let mainID = mainPageHistory.id ?? UUID()
+                                if player != innerPlayer {
+                                    HStack {
+                                        let sum: Int32 = coreDM.fetchSpecificPlayerToPlayerCost(mainName: mainName, enemyName: enemyName, id: mainID)
+                                        Text(innerPlayer.name ?? "")
+                                        Text("에게")
+                                        Text("\(sum)")
+                                        Text("을 받아야 합니다")
+                                        Spacer()
+                                    }
+                                }
+                                
+                            }
+                        }
+                        .padding()
+                        .background (
+                            RoundedRectangle(cornerRadius: 18)
+                                .foregroundColor(.white)
+                                .shadow(color: .gray, radius: 5, x: 0, y: 3)
+                        )
+                    }
+                }
+                .padding(.horizontal)
+                .padding(.top)
+                .frame(maxWidth: .infinity)
+            }
+        }
+        .navigationBarHidden(true)
     }
 }
 
@@ -22,7 +92,18 @@ struct CalculateView_Previews: PreviewProvider {
     static var previews: some View {
         let context = CoreDataManager().persistentContainer.viewContext
         let testHistory = MainPageHistory(context: context)
+        let players = ["플레이어1","플레이어2","플레이어3","플레이어4"]
         
-        CalculateView(mainPageHistory: testHistory)
+        
+        for i in 0 ..< 4 {
+            let player = Player(context: context)
+            player.name = players[i]
+            testHistory.addToPlayer(player)
+        }
+        
+        testHistory.historyName = "2021.09.01"
+        
+        return CalculateView(coreDM: CoreDataManager(), mainPageHistory: testHistory)
+            .environment(\.managedObjectContext, context)
     }
 }
