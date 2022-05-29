0925ec8b71512dcc58d1bdfac74f9a61e0dee233
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-05-30 04:37:44 +0900

    계산 뷰 생성

---
+++ GoStop/GoStop/View/CalculateScoreView.swift
@@ -0,0 +1,20 @@
+//
+//  CalculateScoreView.swift
+//  GoStop
+//
+//  Created by 이태현 on 2022/05/18.
+//
+
+import SwiftUI
+
+struct CalculateScoreView: View {
+    var body: some View {
+        Text("CalculateScoreView")
+    }
+}
+
+struct CalculateScoreView_Previews: PreviewProvider {
+    static var previews: some View {
+        CalculateScoreView()
+    }
+}
