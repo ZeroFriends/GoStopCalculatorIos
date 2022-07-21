829fd937ab1afe368ff9a60438c009ae154b92f9
taehyunL <76516280+taehyunL@users.noreply.github.com>
2022-07-21 19:07:26 +0900

    컴파일러의 타입추론 오류로 인한 view 분리

--- GoStop/GoStop/View/RoundDetailView.swift
+++ GoStop/GoStop/View/RoundDetailView.swift
@@ -18,6 +18,58 @@ struct RoundDetailView: View {
 //        ingamePlayers = coreDM.fetchIngamePlayers(id: mainPageHistory.id!, roundId: round.roundId!)
 //    }
     
+    struct MainIngamePlayerView: View {
+        let ingamePlayerList: [IngamePlayer]
+        let ingamePlayer: IngamePlayer
+        
+        var body: some View {
+            HStack {
+                Text("\(ingamePlayerList.firstIndex(of: ingamePlayer)!+1)\t")
+                    .font(.system(size: 16, weight: .bold))
+                    .foregroundColor(.red)
+                HStack {
+                Text(ingamePlayer.name ?? "no name")
+                    .font(.system(size: 16, weight: .bold))
+                    if ingamePlayer.winner == true {
+                        Text("승")
+                            .font(.system(size: 10, weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(3)
+                            .background(
+                                Circle()
+                                    .foregroundColor(.red)
+                            )
+                    }
+                    if ingamePlayer.seller == true {
+                        Text("광")
+                            .font(.system(size: 10, weight: .bold))
+                            .foregroundColor(.white)
+                            .padding(3)
+                            .background(
+                                Circle()
+                                    .foregroundColor(.green)
+                            )
+                    }
+                }
+                Spacer()
+                if ingamePlayer.totalCost > 0 {
+                    Text("+\(ingamePlayer.totalCost)원")
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundColor(.red)
+                } else if ingamePlayer.totalCost < 0 {
+                    Text("\(ingamePlayer.totalCost)원")
+                        .font(.system(size: 16, weight: .bold))
+                        .foregroundColor(.blue)
+                } else {
+                    Text("\(ingamePlayer.totalCost)원")
+                        .font(.system(size: 16, weight: .bold))
+                }
+            }//HStack
+            .padding(.bottom, 3)
+        }
+        
+    }
+    
     var body: some View {
         VStack {
             HStack {
@@ -47,31 +99,11 @@ struct RoundDetailView: View {
             ScrollView {
                 VStack {
                     ForEach(round.ingamePlayerList, id: \.self) { ingamePlayer in
-                        let inner = ingamePlayer.innerArray
                         
                         VStack {
-                            HStack {
-                                Text("\(round.ingamePlayerList.firstIndex(of: ingamePlayer)!+1)\t")
-                                    .font(.system(size: 16, weight: .bold))
-                                    .foregroundColor(.red)
-                                Text(ingamePlayer.name ?? "no name")
-                                    .font(.system(size: 16, weight: .bold))
-                                Spacer()
-                                if ingamePlayer.totalCost > 0 {
-                                    Text("+\(ingamePlayer.totalCost)원")
-                                        .font(.system(size: 16, weight: .bold))
-                                        .foregroundColor(.red)
-                                } else if ingamePlayer.totalCost < 0 {
-                                    Text("\(ingamePlayer.totalCost)원")
-                                        .font(.system(size: 16, weight: .bold))
-                                        .foregroundColor(.blue)
-                                } else {
-                                    Text("\(ingamePlayer.totalCost)원")
-                                        .font(.system(size: 16, weight: .bold))
-                                }
-                            }//HStack
-                            .padding(.bottom, 3)
+                            MainIngamePlayerView(ingamePlayerList: round.ingamePlayerList, ingamePlayer: ingamePlayer)
                             
+                            let inner = ingamePlayer.innerArray
                             ForEach(inner, id: \.self) { innerIngamePlayer in
 
                                 let cost = innerIngamePlayer.cost
@@ -120,34 +152,34 @@ struct RoundDetailView: View {
     }
 }
 
-struct RoundDetailView_Previews: PreviewProvider {
-    static var previews: some View {
-        let context = CoreDataManager().persistentContainer.viewContext
-        let testHistory = MainPageHistory(context: context)
-        let players = ["플레이어1","플레이어2","플레이어3","플레이어4"]
-        let round = Round(context: context)
-        
-        for i in 0 ..< 4 {
-            let player = Player(context: context)
-            player.name = players[i]
-            testHistory.addToPlayer(player)
-        }
-        testHistory.id = UUID()
-        
-        round.id = testHistory.id
-        
-        for i in 0 ..< 4 {
-            let player = IngamePlayer(context: context)
-            player.name = players[i]
-            player.id = round.id
-            player.totalCost = 0
-            round.addToIngamePlayer(player)
-        }
-        
-        testHistory.addToRound(round)
-        testHistory.historyName = "2021.09.01"
-        
-        return RoundDetailView(coreDM: CoreDataManager(), mainPageHistory: testHistory, round: round)
-            .environment(\.managedObjectContext, context)
-    }
-}
+//struct RoundDetailView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        let context = CoreDataManager().persistentContainer.viewContext
+//        let testHistory = MainPageHistory(context: context)
+//        let players = ["플레이어1","플레이어2","플레이어3","플레이어4"]
+//        let round = Round(context: context)
+//
+//        for i in 0 ..< 4 {
+//            let player = Player(context: context)
+//            player.name = players[i]
+//            testHistory.addToPlayer(player)
+//        }
+//        testHistory.id = UUID()
+//
+//        round.id = testHistory.id
+//
+//        for i in 0 ..< 4 {
+//            let player = IngamePlayer(context: context)
+//            player.name = players[i]
+//            player.id = round.id
+//            player.totalCost = 0
+//            round.addToIngamePlayer(player)
+//        }
+//
+//        testHistory.addToRound(round)
+//        testHistory.historyName = "2021.09.01"
+//
+//        return RoundDetailView(coreDM: CoreDataManager(), mainPageHistory: testHistory, round: round)
+//            .environment(\.managedObjectContext, context)
+//    }
+//}
